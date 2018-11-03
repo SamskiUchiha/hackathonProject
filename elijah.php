d36c14a4eeb5fa5276eebf3ecaaf5f35bf69e705
Elijah Baird <ebaird@csumb.edu>
2018-11-03 18:07:09 +0000

    added a small amount of css and a file containing what i added to index but it does not work yet

---
+++ elijah.php
@@ -0,0 +1,111 @@
+<?php
+
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+}
+// Check if file already exists
+// if (file_exists($target_file)) {
+//     echo "Sorry, file already exists.";
+//     $uploadOk = 0;
+// }
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 5000000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
+    echo "Sorry, only JPG, JPEG, & PNG files are allowed.";
+    $uploadOk = 0;
+}
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+}
+
+function add($data)
+{
+    printf($data);
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title> MapIt </title>
+        <link href="css/styles.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <div id="title">MapIt</div>
+        <br>
+        <form>
+            Enter a number: <input type="number" name="data"/>
+            <br><br>
+            Choose a Disaster: 
+            <select>
+                <option value="earthquake">Earthquake</option>
+                <option value="floods">Flood</option>
+                <option value="blackouts">Blackout</option>
+                <option value="sinkhole">Sinkhole</option>
+                <option value="mudslide">Mudslide</option>
+                <option value="thunderstorm">Thunderstorm</option>
+                <option value="tornado">Tornado</option>
+                <option value="blizzard">Blizzard</option>
+                <option value="avalanche">Avalanche</option>
+                <option value="volcano">Volcanic Eruption</option>
+                <option value="tsunami">Tsunami</option>
+            </select>
+            <br><br>
+            Choose the location nearest to you: 
+            <select>
+                <option value="csumb">CSUMB</option>
+                <option value="sandCity">Sand City</option>
+                <option value="monterey">Monterey</option>
+                <option value="salinas">Salinas</option>
+                <option value="carmel">Carmel</option>
+                <option value="marina">Marina</option>
+                <option value="seaside">Seaside</option>
+            </select>
+            <br><br>
+        </form>
+        <form action="index.php" method="post" enctype="multipart/form-data">
+            Select image to upload:
+            <input type="file" name="fileToUpload" id="fileToUpload">
+            <br><br>
+            <input type="submit" value="Submit" name="submit">
+        </form>
+            <!--Select an image to upload: -->
+            <!--<input type="file" name="fileupload" value="fileupload" id="fileupload"> -->
+            <!--<label for="fileupload"></label> -->
+            <!--<br>-->
+            <!--<input type="image" src="/wp-content/uploads/sendform.png" alt="Submit" width="100">-->
+
+            <!--<br><br>-->
+            <!--<input type="submit" value="Submit"/>-->
+            <!--<br>-->
+            <!--<?php //add($_GET["fileupload"]); ?>-->
+        
+        
+    </body>
+</html>
