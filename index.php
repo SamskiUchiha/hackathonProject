b00cfc7d714f8cd4aefb92e9b44e89d1be3b0e11
Santiago Bruno <sbrunogonzalez@csumb.edu>
2018-11-03 18:11:19 +0000

    centering map to CSUMB

--- index.php
+++ index.php
@@ -30,8 +30,8 @@
       var map;
       function initMap() {
         map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 8
+          center: {lat: 36.652658, lng: -121.797381},
+          zoom: 12
         });
       }
     </script>
